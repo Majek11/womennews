ecca6f322a2dbea50423b1d7d0b0e10059356fe1
Majek11 <majeklekan9@gmail.com>
2023-03-05 14:52:40 -0800

    socials fixed

--- src/components/home/sideContent/socials/SocialMedia.jsx
+++ src/components/home/sideContent/socials/SocialMedia.jsx
@@ -6,23 +6,30 @@ const SocialMedia = () => {
       <section className='social'>
         <div className='socBox'>
           <i className='fab fa-facebook-f'></i>
-          <span>12,740 Likes</span>
+  
+          <span><a href="https://www.facebook.com/profile.php?id=100090445105818&mibextid=ZbWKwL" target="_blank">Facebook</a></span>
         </div>
         <div className='socBox'>
-          <i className='fab fa-pinterest'></i>
-          <span>5,600 Fans</span>
+          <i className='fab fa-snapchat'></i>
+          <span><a href="https://www.snapchat.com/add/womeninnews?share_id=DPqvIzkUIhw&locale=en-NG" target="_blank">SnapChat</a></span>
         </div>
         <div className='socBox'>
           <i className='fab fa-twitter'></i>
-          <span>8,700 Followers</span>
+          <span>
+            <a href="https://twitter.com/thewomeninnews?t=iOc6wrtRDJVLPPdZBg2tfA&s=09" target="_blank">Twitter</a>
+          </span>
         </div>
         <div className='socBox'>
           <i className='fab fa-instagram'></i>
-          <span>22,700 Followers</span>
+          <span>
+            <a href=" https://www.instagram.com/invites/contact/?i=a9o8knyqcyfo&utm_content=qwize1s" target="_blank">Instagram</a>
+          </span>
         </div>
         <div className='socBox'>
-          <i className='fab fa-youtube'></i>
-          <span>2,700 Subscriber</span>
+          <i className='fab fa-tiktok'></i>
+          <span>
+            <a href="https://www.tiktok.com/@womenvcwvma?_t=8aMZbkRWihx&_r=1" target="_blank">TikTok</a>
+          </span>
         </div>
       </section>
     </>
